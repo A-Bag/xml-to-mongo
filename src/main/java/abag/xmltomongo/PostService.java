dd6a2c0b1ffef2ba74c9b24cfa91f300dba151b2
Adam Bagrowski <bagrowski.adam@gmail.com>
2019-11-27 13:19:28 +0100

    Implementation

---
+++ src/main/java/abag/xmltomongo/PostService.java
@@ -0,0 +1,129 @@
+package abag.xmltomongo;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.text.StringEscapeUtils;
+import org.jsoup.Jsoup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Value("${bulk.insert.size}")
+    private int batchSize;
+
+    @Value("${xml.file.location}")
+    private String fileLocation;
+
+    private MongoTemplate mongoTemplate;
+
+    public PostService(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void saveXmlData() throws IOException {
+
+        int rowNumber = 0;
+
+        try (LineIterator lineIterator = FileUtils.lineIterator(new File(fileLocation))) {
+
+            Instant saveStart = Instant.now();
+
+            List<Post> posts = new ArrayList<>();
+
+            while (lineIterator.hasNext()) {
+                String line = lineIterator.nextLine();
+                if (line.contains("<row")) {
+
+                    Post post = convertXmlLineToPost(line);
+                    posts.add(post);
+
+                    rowNumber++;
+
+                    if (rowNumber % batchSize == 0) {
+                        mongoTemplate.insert(posts, Post.class);
+                        posts.clear();
+                    }
+
+                    if (rowNumber % 100_000 == 0) {
+                        System.out.println(rowNumber + " / 10 000 000 rows inserted");
+                    }
+
+                    if (rowNumber == 10_000_000) {
+                        break;
+                    }
+                }
+            }
+            Instant saveEnd = Instant.now();
+
+            long saveTime = Duration.between(saveStart, saveEnd).toMillis();
+
+            System.out.println("Save time: " + saveTime + "ms");
+
+        }
+    }
+
+    public Post convertXmlLineToPost(String line) {
+        Post post = new Post();
+
+        post.setId(Integer.parseInt(extractStringField("Id", line)));
+
+        if (line.contains("PostTypeId=\"")) {
+            post.setPostType(Integer.parseInt(extractStringField("PostTypeId=\"", line)));
+        }
+
+        if (line.contains("Score=\"")) {
+            post.setScore(Integer.parseInt(extractStringField("Score", line)));
+        }
+
+        if (line.contains("ViewCount=\"")) {
+            post.setViewCount(Integer.parseInt(extractStringField("ViewCount=\"", line)));
+        }
+
+        if (line.contains("CreationDate=\"")) {
+            String date = extractStringField("CreationDate=\"", line);
+            int year = Integer.parseInt(date.substring(0, 4));
+            post.setYear(year);
+        }
+
+        if (line.contains("Body=\"")) {
+            String rawBody = extractStringField("Body=\"", line);
+            String bodyText = removeHTMLFromString(rawBody);
+            post.setBody(bodyText);
+        }
+
+        if (line.contains("Title=\"")) {
+            String rawTitle = extractStringField("Title=\"", line);
+            String titleText = removeHTMLFromString(rawTitle);
+            post.setTitle(titleText);
+        }
+
+        return post;
+    }
+
+    public String extractStringField(String fieldName, String line) {
+        int beginFieldIndex = line.indexOf(fieldName);
+        int beginIndex = line.indexOf("\"", beginFieldIndex) + 1;
+        int endIndex = line.indexOf("\"", beginIndex);
+        return line.substring(beginIndex, endIndex);
+    }
+
+    private String removeHTMLFromString(String input) {
+        String replace = input.replace("&amp;", "");
+        String html = StringEscapeUtils.unescapeHtml4(replace);
+        return Jsoup.parse(html).text();
+    }
+
+}
