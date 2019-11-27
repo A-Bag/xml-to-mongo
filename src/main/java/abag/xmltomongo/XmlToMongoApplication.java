6e07b38634a0be8e2ecfac9e1497127dd5df1cc2
Adam Bagrowski <a.bagrowski@ailleron.com>
2019-11-27 08:50:09 +0100

    initial commit

---
+++ src/main/java/abag/xmltomongo/XmlToMongoApplication.java
@@ -0,0 +1,13 @@
+package abag.xmltomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlToMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlToMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/abag/xmltomongo/XmlToMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package abag.xmltomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlToMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
